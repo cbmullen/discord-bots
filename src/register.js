101af1dce5adfa8c50547d9c9199e9ba8fd722ba
cbmullen <cbmullen@gmail.com>
2024-04-11 15:06:37 +0100

    fix: fix register

--- src/register.js
+++ src/register.js
@@ -1,4 +1,4 @@
-import { PBEM_COMMAND, INVITE_COMMAND } from './commands.js';
+import { PBEM_COMMAND } from './commands.js';
 import dotenv from 'dotenv';
 import process from 'node:process';
 
@@ -34,7 +34,7 @@ const response = await fetch(url, {
     Authorization: `Bot ${token}`,
   },
   method: 'PUT',
-  body: JSON.stringify([PBEM_COMMAND, INVITE_COMMAND]),
+  body: JSON.stringify([PBEM_COMMAND]),
 });
 
 if (response.ok) {
